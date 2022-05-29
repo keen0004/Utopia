c549465308a1f2eb531d743371828548990b33be
Peter <fuxideng@gmail.com>
2022-05-29 10:47:22 +0800

    add cmc tool

--- cmd/cointool/command.go
+++ cmd/cointool/command.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"utopia/internal/cmc"
+
 	"gopkg.in/urfave/cli.v1"
 )
 
 var (
-	xxxFlag = cli.StringFlag{
-		Name:  "xxx",
-		Usage: "xxx",
+	CoinFlag = cli.StringFlag{
+		Name:  "coin",
+		Usage: "Set the coin symbol, empty for all",
+		Value: "",
+	}
+	FileFlag = cli.StringFlag{
+		Name:  "file",
+		Usage: "Set the file path for input or output",
 		Value: "",
 	}
 
@@ -20,7 +30,7 @@ var (
 				Usage:  "Query price of token",
 				Action: QueryPrice,
 				Flags: []cli.Flag{
-					xxxFlag,
+					CoinFlag,
 				},
 			},
 		},
@@ -34,7 +44,8 @@ var (
 				Usage:  "Query coin information on cmc",
 				Action: QueryCoin,
 				Flags: []cli.Flag{
-					xxxFlag,
+					CoinFlag,
+					FileFlag,
 				},
 			},
 		},
@@ -48,7 +59,7 @@ var (
 				Usage:  "Query collection information on cmc",
 				Action: QueryCollection,
 				Flags: []cli.Flag{
-					xxxFlag,
+					CoinFlag,
 				},
 			},
 		},
@@ -60,6 +71,23 @@ func QueryPrice(ctx *cli.Context) error {
 }
 
 func QueryCoin(ctx *cli.Context) error {
+	coin := ctx.String(CoinFlag.Name)
+	//file := ctx.String(FileFlag.Name)
+
+	client := cmc.NewClient(&cmc.Config{ProAPIKey: "10edfcd3-1e9f-4261-bd55-faa366216aa2"})
+	if client == nil {
+		return errors.New("Create cmc client failed")
+	}
+
+	result, err := client.Cryptocurrency.Info(&cmc.InfoOptions{Symbol: coin})
+	if err != nil {
+		return err
+	}
+
+	for symbol, info := range result {
+		fmt.Fprintf(os.Stderr, "[%s] %v", symbol, info)
+	}
+
 	return nil
 }
 
